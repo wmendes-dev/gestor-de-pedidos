8a8ca26f67b915738cb84c1df5dcaf92ba51b4f7
wmendes-dev <whdesenvolvedor@gmail.com>
2025-06-05 22:44:11 -0400

    Ajustes docker-compose e Dockerfile

--- autenticacao-api/src/main/java/br/com/autenticacao_api/configurations/SegurancaConfig.java
+++ autenticacao-api/src/main/java/br/com/autenticacao_api/configurations/SegurancaConfig.java
@@ -67,7 +67,7 @@ public class SegurancaConfig {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:8005"));
+        configuration.setAllowedOrigins(List.of("http://localhost:8080"));
         configuration.setAllowedMethods(List.of("GET", "POST"));
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
