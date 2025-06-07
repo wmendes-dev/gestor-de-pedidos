29f4f88bc5f19cae6b0ef9e47283aae03742beeb
wmendes-dev <whdesenvolvedor@gmail.com>
2025-06-06 21:43:40 -0400

    Ajustes Estoque API

---
+++ estoque-api/src/main/java/br/com/estoque_api/configurations/JwtDecoderConfig.java
@@ -0,0 +1,24 @@
+package br.com.estoque_api.configurations;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+
+@Configuration
+public class JwtDecoderConfig {
+
+    @Value("${jwt.secret}")
+    private String secretKey;
+
+    @Bean
+    public JwtDecoder jwtDecoder() {
+        SecretKey key = new SecretKeySpec(this.secretKey.getBytes(), "HMACSHA256");
+        return NimbusJwtDecoder.withSecretKey(key).build();
+    }
+
+}
